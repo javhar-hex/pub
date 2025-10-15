21a6819edae67f3b0bdc61da87f9e0958e4b0c03
Jack Javhar <github@javhar.net>
2025-10-14 17:40:06 -0700

    pass args to install func

--- repo_setup.py
+++ repo_setup.py
@@ -1,32 +1,32 @@
 import os
 from importlib.metadata import version as dist_version, PackageNotFoundError
 
-TARGET_VERSION = globals().get("target_version", "0.1.24")
-FORCE_INSTALL = globals().get("force_install", False)
-GITHUB_PAT = globals().get("github_pat")
-DIST_NAME = globals().get("dist_name", "javhar")
-
 def installed_dist_version():
     try:
         return dist_version(DIST_NAME)
     except PackageNotFoundError:
         return None
 
-def get_token():
+def get_token(github_pat: str):
     from google.colab import userdata
     try:
-        return userdata.get(GITHUB_PAT)
+        return userdata.get(github_pat)
     except KeyError:
         raise KeyError("Error: GitHub PAT secret not found. Check the Secrets sidebar (key icon)!")
 
-def install():
+def install(
+    github_pat: str, 
+    target_version: str = "0.1.24",
+    force_install: bool = False,
+    dist_name: str = "javhar",
+):
     print("hee kijk nou! pub repo_setup.py install")
     print(f"dist version  : {installed_dist_version()}")
-    print(f"target version: {TARGET_VERSION} (set 'target_version')")
-    print(f"force install?  {FORCE_INSTALL} (set 'force_install')")
+    print(f"target version: {target_version}")
+    print(f"force install?  {force_install}")
 
-    if FORCE_INSTALL or installed_dist_version() != TARGET_VERSION:
-        token = get_token()
+    if force_install or installed_dist_version() != target_version:
+        token = get_token(github_pat)
         url = f"git+https://x-access-token:{token}@github.com/javhar-hex/hex.git@main"
         install_command = (
             f"pip install -q "
@@ -44,5 +44,3 @@ def install():
         print("dist version is now ", installed_dist_version())
     else:
         print("nothing to do.")
-    
-    print("done.")
